28ce1e68e9bfc3f0b538e67fe9f04096e9e8846a
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-09-14 12:32:41 +0530

    Campus table modif added

--- lab_2/update-db.py
+++ lab_2/update-db.py
@@ -28,6 +28,7 @@ except mysql.connector.errors.ProgrammingError:
 mycursor = mydb.cursor()
 
 # Changes requested in current lab for table Student
+
 mycursor.execute("alter table Student add gender char(1) default 'M';")
 mycursor.execute("alter table Student add team char(1);")
 mycursor.execute("alter table Student drop column age;")
@@ -41,7 +42,7 @@ mycursor.execute("ALTER TABLE Student MODIFY yjoin int AFTER dob;")
 mycursor.execute("ALTER TABLE Student MODIFY gender char(1) AFTER yjoin;")
 mycursor.execute("ALTER TABLE Student MODIFY team char(1) AFTER gender;")
 
-prev_Campus = {
+prev_students = {
 	'name': ["dwayne", "john", "dave", "randy", "kane", "tom"],
 	'dob': ['2000-01-09', '2002-03-21', '1999-09-11', '2001-04-28', '2000-11-01', '2000-11-01'],
 	'yjoin': [2019, 2019, 2018, 2019, 2018, 2019],
@@ -50,10 +51,10 @@ prev_Campus = {
 
 for i in range(6):
 	mycursor.execute(f"""UPDATE Student SET
-		dob=STR_TO_DATE('{prev_Campus["dob"][i]}', '%Y-%m-%d'),
-		yjoin={prev_Campus["yjoin"][i]},
-		team='{prev_Campus["team"][i]}'
-		WHERE name LIKE '{prev_Campus["name"][i]}';""")
+		dob=STR_TO_DATE('{prev_students["dob"][i]}', '%Y-%m-%d'),
+		yjoin={prev_students["yjoin"][i]},
+		team='{prev_students["team"][i]}'
+		WHERE name LIKE '{prev_students["name"][i]}';""")
 
 mydb.commit()
 
@@ -75,7 +76,7 @@ for i in range(5):
 	mycursor.execute(f"""
 		INSERT INTO Student VALUES ('{new_students["name"][i]}',
 		{new_students["rollnos"][i]},
-		STR_TO_DATE('{prev_Campus["dob"][i]}', '%Y-%m-%d'),
+		STR_TO_DATE('{new_students["dob"][i]}', '%Y-%m-%d'),
 		{new_students["yjoin"][i]},
 		'F',
 		'{new_students["team"][i]}',
@@ -85,6 +86,52 @@ for i in range(5):
 		{new_students["social"][i]},
 		{new_students["sports"][i]}
 		);
+	""")
+
+mydb.commit()
+
+
+# Changes requested in current lab for Campus table
+
+mycursor.execute("ALTER TABLE Campus ADD pincode char(6)")
+
+prev_campus = {
+	'names': ['mec', 'muc', 'mgt'],
+	'pincode': ['500043', '600012', '400076']
+}
+
+for i in range(3):
+	mycursor.execute(f"""UPDATE Campus SET
+		pincode='{prev_campus["pincode"][i]}'
+		WHERE name LIKE '{prev_campus["names"][i]}';
+	""")
+
+mydb.commit()
+
+mycursor.execute("ALTER TABLE Campus MODIFY pincode char(6) AFTER loc;")
+
+new_campus = {
+	'names': ['mdc', 'mdd', 'mvc', 'mbd'],
+	'cid': [106, 109, 110, 11],
+	'loc': ['pun', 'del', 'vel', 'bgl'],
+	'pincode': ['411021', '110001', '631001', '560002'],
+	'cap': [1700, 1400, 1400, 800],
+	'law': [0, 1, 1, 0],
+	'engg': [1, 1, 1, 1],
+	'buss': [1, 0, 1, 1]
+}
+
+for i in range(4):
+	mycursor.execute(f"""INSERT INTO Campus VALUES (
+		'{new_campus["names"][i]}',
+		{new_campus["cid"][i]},
+		'{new_campus["loc"][i]}',
+		'{new_campus["pincode"][i]}',
+		{new_campus["cap"][i]},
+		{new_campus["law"][i]},
+		{new_campus["engg"][i]},
+		{new_campus["buss"][i]}
+	);
 	""")
 
 mydb.commit()
