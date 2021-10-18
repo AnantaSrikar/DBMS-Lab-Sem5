7843e1cf8e9001477ec8786bbb41a63bb159c115
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-10-18 22:06:41 +0530

    Added 4th sol

--- lab_6/assign6.sql
+++ lab_6/assign6.sql
@@ -7,4 +7,6 @@ SELECT name, marks_sem1.sports FROM Student JOIN marks_sem1 ON Student.rollno=ma
 -- q3
 SELECT Student.name, marks_sem1.math, Student.rollno FROM Student JOIN Campus ON Student.cid=Campus.cid JOIN marks_sem1 ON Student.rollno=marks_sem1.rollno WHERE Campus.law=1;
 
--- q4
+-- q4 (NOTE: Always have ON clause for LEFT and RIGHT joins. And save some debugging time.)
+SELECT * FROM Student LEFT JOIN Campus ON Student.cid=Campus.cid;
+SELECT * FROM Student RIGHT JOIN Campus ON Student.cid=Campus.cid;
